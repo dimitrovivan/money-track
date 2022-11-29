355aafed5bf9fbbbc4f8a8f7b678586060809eef
idimitrov <idimitrov@htmlbox.net>
2022-11-29 13:51:32 +0200

    Add logo and modify base font size

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,6 +1,14 @@
+import Logo from '@/svg/logo.svg';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <div className="flex justify-center">
+        <div className="max-w-xs">
+          <Logo className="w-full" />
+        </div>
+      </div>
+    </div>
   )
 }
 
